866c4c2363936743a1509f42117573638a299c51
drunisa007 <newarunkatwal@gmail.com>
2022-07-08 17:05:49 +0630

    Still needed to do some function

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,16 @@
 //     println!("hello from rust");
 // }
 
-
-pub fn print_difference(x:f32,y:f32){
-    println!("Different between {} and {} is {}",x,y,(x-y).abs());
+pub fn print_difference(x: f32, y: f32) {
+    println!("Different between {} and {} is {}", x, y, (x - y).abs());
 }
 
-pub fn print_array(x:[f64;2]){
-  println!("the coordinates are {} {}",x[0],x[1]);
+pub fn print_array(x: [f64; 2]) {
+    println!("the coordinates are {} {}", x[0], x[1]);
 }
 
-
-pub fn check_13(number:i32){
-    if number==13{
+pub fn check_13(number: i32) {
+    if number == 13 {
         println!("yes this is 13 yayyyy");
     }
 }
