8a7feac7a142527d52f4b2adc0672b53ea58b46b
drunisa007 <newarunkatwal@gmail.com>
2022-07-08 12:46:18 +0630

    done exercise 2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,45 @@
-const TOTAL_MISSLE:i32 = 8;
-const TOTAL_READY:i32 = 2;
+// const TOTAL_MISSLE:i32 = 8;
+// const TOTAL_READY:i32 = 2;
 
+#![allow(unused_variables)]
 fn main(){
-   let mut missle:i32 = TOTAL_MISSLE;
-   let ready:i32  = TOTAL_READY;
-
-   missle = missle - ready;
 
 
-   println!("Shot {} missles and remain {} missles", ready,missle);
+   let width = 4;
+   let height = 7;
+   let depth = 10;
+
+
+   let area = area_of(width, height);
+
+   println!("area is {} !",area);
+
+
+   let volume = volume_of(width, height, depth);
+
+   println!("volume is {}",volume);
+
+
+   fn volume_of(width: i32,height: i32,depth:i32) -> i32 {
+      width*height*depth
+   }
+
+
+   fn area_of(width: i32,height: i32) -> i32{
+      width*height
+   }
+
+
+
+
+
+   //exercise one
+
+   // let mut missle:i32 = TOTAL_MISSLE;
+   // let ready:i32  = TOTAL_READY;
+
+   // missle = missle - ready;
+
+
+   // println!("Shot {} missles and remain {} missles", ready,missle);
 }
